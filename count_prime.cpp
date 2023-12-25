b70d95f9833d26ac0fb5cfc332ee4a7ddfd254e2
Deependu Mishra <144558379+mishradeep18@users.noreply.github.com>
2023-12-26 01:33:54 +0530

    Create count_prime.cpp

---
+++ count_prime.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool>prime (n+2,true);
+        int cnt = 0;
+        prime[0] = prime[1] = 0;
+        for(int i = 2; i<n; i++){
+            if(prime[i]){
+                cnt++;
+
+                for(int j = 2*i; j<n; j = j+i){
+                    prime[j]= 0;
+                }
+            }
+            
+        }
+        return cnt;
+
+        
+    }
+};
