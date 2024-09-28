a2fe86448d000750be6631698791e8251c61da57
Deependu Mishra <144558379+mishradeep18@users.noreply.github.com>
2024-09-28 10:50:33 +0530

    Add files via upload

---
+++ secondLargest.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void getElement(int arr[] , int n )
+{
+    if(n==0 || n == 1){              //edge condition
+        cout<<-1<<''<<-1<<endl;
+    }
+}
+
+int main(){
+
+}
